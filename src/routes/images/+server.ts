87bed8cc1aafabf8f66fa0a206f4c4eca4e5b819
fcastrovilli <francesco.castrovilli95@gmail.com>
2023-10-24 22:15:03 +0200

    feat: vercel-edge-function

--- src/routes/images/+server.ts
+++ src/routes/images/+server.ts
@@ -1,25 +1,19 @@
-// import resemble from 'resemblejs';
-// let last_url: string | undefined = undefined;
+import { error } from '@sveltejs/kit';
+
+export const config: import('@sveltejs/adapter-vercel').Config = {
+	runtime: 'edge'
+};
 
 export async function GET({ setHeaders, url, fetch }) {
-	// try {
-	const toFetch = String(url.searchParams.get('url'));
-	const res = await fetch(toFetch);
-	const blob = await res.blob();
-	// if (last_url !== undefined) {
-	// 	resemble(last_url)
-	// 		.compareTo(toFetch)
-	// 		.ignoreColors()
-	// 		.onComplete(function (data) {
-	// 			console.log(data);
-	// 		});
-	// }
-	// last_url = toFetch;
-	setHeaders({
-		'Content-Type': 'image/png'
-	});
-	return new Response(blob);
-	// } catch (e) {
-	// 	throw error(500, 'Internal server error');
-	// }
+	try {
+		const toFetch = String(url.searchParams.get('url'));
+		const res = await fetch(toFetch);
+		const blob = await res.blob();
+		setHeaders({
+			'Content-Type': 'image/png'
+		});
+		return new Response(blob);
+	} catch (e) {
+		throw error(500, 'Internal server error');
+	}
 }
