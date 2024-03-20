b82251f5bd55c0bd81650b986500ea6aaa4fb681
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-03-20 16:05:03 +0100

    feat: analyze

---
+++ src/lib/components/canvas-analyze.svelte
@@ -0,0 +1,101 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import { all_pins, scrollPosition, bookmark } from '$lib/stores';
+	import { onMount } from 'svelte';
+	import { Image as imagejs } from 'image-js';
+
+	let canvas: HTMLCanvasElement;
+	let analyzed_img: HTMLImageElement;
+	let ctx: CanvasRenderingContext2D | null = null;
+	let img: HTMLImageElement;
+	let gap: number = 15;
+	let btn: HTMLButtonElement;
+	let y: number = 0;
+	$: $scrollPosition = y;
+	$: url = '/images?url=' + $all_pins[$scrollPosition].images['orig'].url;
+
+	//////////////////////////
+	onMount(async () => {
+		ctx = canvas.getContext('2d');
+		img = new Image();
+		img.src = url;
+	});
+	$: {
+		if (ctx) {
+			img.src = url;
+			img.onload = () => {
+				draw();
+			};
+		}
+	}
+
+	function draw() {
+		canvas.width = $all_pins[$scrollPosition].images['orig'].width;
+		canvas.height = $all_pins[$scrollPosition].images['orig'].height;
+		ctx?.drawImage(img, 0, 0, canvas.width, canvas.height);
+		analyze();
+	}
+	function refetch() {
+		btn.click();
+	}
+
+	async function analyze() {
+		let toAnalyze = await imagejs.load(url);
+		let gaussian = toAnalyze.grey().gaussianFilter({ radius: 8 });
+		let mask = gaussian.mask({ threshold: 0.5 });
+		let roiManager = toAnalyze.getRoiManager();
+		roiManager.fromMask(mask);
+		let painted = roiManager.paint({
+			// @ts-ignore
+			distinctColor: true,
+			positive: true,
+			negative: true,
+			alpha: 127,
+			labelProperty: 'surface',
+			labelColor: 'white',
+			unit: 'px'
+		});
+		analyzed_img.src = painted.toDataURL();
+		analyzed_img.style.opacity = '0.7';
+	}
+
+	function onKeyDown(e: KeyboardEvent) {
+		if (e.key == 'ArrowUp' && y == 0) return;
+		if (e.key == 'ArrowDown' && y == $all_pins.length - 1) return;
+		if (e.key == 'ArrowDown' && y == $all_pins.length - gap) {
+			y = $all_pins.length - gap + 1;
+			refetch();
+		} else if (e.key == 'ArrowDown') y += 1;
+		if (e.key == 'ArrowUp') y -= 1;
+	}
+</script>
+
+<svelte:window on:keydown={onKeyDown} />
+<div class="w-screen h-screen overflow-hidden">
+	<form
+		method="post"
+		use:enhance={() => {
+			return async ({ result }) => {
+				// @ts-ignore
+				if (!result.data) return;
+				// @ts-ignore
+				$all_pins = [...result.data.pins];
+				// @ts-ignore
+				$bookmark = result.data.bookmark;
+				console.log('refetched');
+			};
+		}}
+		action="?/refetch"
+	>
+		<input type="text" class="hidden" name="bookmark" value={$bookmark} />
+		<button bind:this={btn} type="submit" class="hidden" />
+	</form>
+	<div class="h-screen w-screen flex justify-center items-center">
+		<canvas bind:this={canvas} class="max-h-screen object-contain h-full w-full" />
+		<img
+			bind:this={analyzed_img}
+			class="absolute max-h-screen object-contain h-full w-full"
+			alt=""
+		/>
+	</div>
+</div>
--- src/routes/analyze/+page.svelte
+++ src/routes/analyze/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { all_pins, bookmark } from '$lib/stores';
 	import type { PinResponse } from '$lib/types.js';
-	import Canvas2 from '$lib/components/canvas2.svelte';
+	import Canvas from '$lib/components/canvas-analyze.svelte';
 
 	////////// INIT //////////
 	export let data;
@@ -9,24 +9,14 @@
 	$all_pins = pins;
 	let last_bookmark = data.bookmark;
 	$bookmark = last_bookmark;
-
-	let started: boolean = false;
 </script>
 
-{#if started}
-	{#if $all_pins}
-		{#if $all_pins.length > 0}
-			<Canvas2 />
-		{:else}
-			<div class="flex justify-center items-center h-screen">
-				<p class="text-white">no data found</p>
-			</div>
-		{/if}
+{#if $all_pins}
+	{#if $all_pins.length > 0}
+		<Canvas />
+	{:else}
+		<div class="flex justify-center items-center h-screen">
+			<p class="text-white">no data found</p>
+		</div>
 	{/if}
-{:else}
-	<div class="w-full h-full flex justify-center items-center">
-		<button on:click={() => (started = true)} class="p-4 rounded-lg border border-white text-white">
-			START
-		</button>
-	</div>
 {/if}
