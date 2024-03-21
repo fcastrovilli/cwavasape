c3806af58c7173756e0aec48d1a399b74d0844e9
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-03-21 23:34:55 +0100

    feat: giacomo fine tuning

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,16 +3,16 @@ import { Image as imagejs } from 'image-js';
 export async function analyze(url: string) {
 	if (!url) return;
 	const toAnalyze = await imagejs.load(url);
-	const gaussian = toAnalyze.grey().gaussianFilter({ radius: 4 });
+	const gaussian = toAnalyze.grey().gaussianFilter({ radius: 1 });
 	const mask = gaussian.mask({ threshold: 0.5 });
 	const roiManager = toAnalyze.getRoiManager();
 	roiManager.fromMask(mask);
 	const painted = roiManager.paint({
 		// @ts-expect-error - TS doesn't like the fact that we're using a string here
-		distinctColor: true,
-		positive: false,
-		negative: true,
-		alpha: 800,
+		distinctColor: false,
+		positive: true,
+		negative: false,
+		alpha: 1000,
 		labelProperty: 'surface',
 		labelColor: 'black',
 		unit: 'px'
